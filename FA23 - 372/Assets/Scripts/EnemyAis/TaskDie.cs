b811811afe49bc2bcbc89499f379bd5eb325df18
JimothyZ <ZajkowskJD13@uww.edu>
2023-12-07 15:08:22 -0600

    try this

--- FA23 - 372/Assets/Scripts/EnemyAis/TaskDie.cs
+++ FA23 - 372/Assets/Scripts/EnemyAis/TaskDie.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BehaviorTree;
-
+using UnityEngine.AI;
 public class TaskDie : EnemyNode
 {
     GameObject enemy;
     Animator animator;
-    public TaskDie(GameObject enemy, Animator animator) { 
+    NavMeshAgent agent;
+
+    public TaskDie(GameObject enemy, Animator animator, NavMeshAgent agent) { 
         this.enemy = enemy;
         this.animator = animator;
+        this.agent = agent;
     }
 
     public override NodeState Evaluate()
@@ -17,6 +20,7 @@ public class TaskDie : EnemyNode
         Debug.Log("Die");
         //do animation things
         animator.SetInteger("state", 3);
+        agent.SetDestination(enemy.transform.position);
         //i think the object has to be destroyed in the enemy health class
         return NodeState.RUNNING;
         
